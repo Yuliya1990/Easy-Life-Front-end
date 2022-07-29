2b950af90d5ae5d3c0551dc101cb324c055f2533
Yulia Solomakha <yulichka700@gmail.com>
2022-07-29 16:39:09 +0200

    First content page is OK

--- src/Components/FirstContent/FirstContent.jsx
+++ src/Components/FirstContent/FirstContent.jsx
@@ -8,14 +8,16 @@ function FirstContent() {
   return (
     <Container fluid style={{ marginTop: '100px' }}>
       <Row className={styles.row}>
-        <Col xs={12} lg={7} sm={6}>
+        <Col xs={12} lg={7} sm={6} className={styles.col}>
           <div className={styles.text}>
-            <h1> Hello!</h1>
+            <h1>
+              Ready to <span className={styles.textEmphasis}>free up</span> your time?
+            </h1>
             <p>Some text</p>
           </div>
         </Col>
-        <Col>
-          <div className={styles.phone}>
+        <Col xs={12} lg={4} sm={6} className={styles.col}>
+          <div>
             <img
               src="https://img.moyo.ua/img/gallery/4978/93/1234982_middle.jpg?1629268509"
               style={{ height: '650px' }}
