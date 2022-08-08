dff25d0dcac080431d5c78b2996f237d90e7ac0e
Yulia Solomakha <yulichka700@gmail.com>
2022-08-08 16:17:12 +0200

    Bigger logo

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -7,7 +7,7 @@ const Logo = () => {
       <div className={styles.WhiteLine1}></div>
       <div className={styles.WhiteLine2}></div>
       <div className={styles.text}>
-        <div style={{ height: '30px', marginTop: -5 }}>EASY</div>
+        <div style={{ height: '40px', marginTop: -5 }}>EASY</div>
         <div style={{ height: '30px' }}>LIFE</div>
       </div>
     </div>
