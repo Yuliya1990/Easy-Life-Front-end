cb73c83070b12f4ac33132d219c95bd15fde8a4d
Yulia Solomakha <yulichka700@gmail.com>
2022-09-22 14:33:58 +0200

    TestCleaningComponents

--- src/Components/Pages/CleaningComponents/CleaningComponents.jsx
+++ src/Components/Pages/CleaningComponents/CleaningComponents.jsx
@@ -1,12 +1,24 @@
 import Navibar from '../../Navibar/Navibar';
 import React from 'react';
+import styles from './CleaningComponents.module.css';
 import { useState, useEffect } from 'react';
 import { createAPIEndpoint, ENDPOINTS } from '../../../api/index';
 import { useParams } from 'react-router-dom';
+import {
+  Button,
+  ToggleButton,
+  ButtonGroup,
+  Container,
+  Row,
+  Col,
+  ToggleButtonGroup,
+} from 'react-bootstrap';
 
 export default function CleaningComponents() {
-  const [components, setComponents] = useState();
+  const [components, setComponents] = useState([]);
+  const [allComponents, setAllComponents] = useState([]);
   const { cleaningTypeid } = useParams();
+
   useEffect(() => {
     createAPIEndpoint(ENDPOINTS.cleaningComponents + '/' + cleaningTypeid)
       .fetch()
@@ -15,11 +27,51 @@ export default function CleaningComponents() {
         console.log(response.data);
       })
       .catch((err) => console.log(err));
+
+    createAPIEndpoint(ENDPOINTS.cleaningComponents)
+      .fetch()
+      .then((response) => {
+        setAllComponents(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
+
+  function isActive(component) {
+    if (components.find((element) => element.name === component.name)) return true;
+    else return false;
+  }
+
   return (
     <>
       <Navibar />
-      <h1 style={{ marginTop: '100px' }}>{cleaningTypeid}</h1>;
+      <Container>
+        <Row>
+          <h2 style={{ marginTop: '120px', marginBottom: '30px' }}>
+            What is included in the cleaning:
+          </h2>
+        </Row>
+        <Row>
+          {allComponents.map((component, index) => {
+            return (
+              <>
+                <Col xs={12} sm={6} lg={4} xl={3}>
+                  <ToggleButton
+                    className={styles.btn}
+                    type="checkbox"
+                    variant="outline-warning"
+                    id={index}
+                    key={index}
+                    value={index}
+                    checked={isActive(component)}>
+                    <h5>{component.name}</h5>
+                  </ToggleButton>
+                </Col>
+              </>
+            );
+          })}
+        </Row>
+      </Container>
     </>
   );
 }
